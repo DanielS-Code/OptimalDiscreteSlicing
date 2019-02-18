5aca8da27f6cd1c285152f45e8cb96015d8eb07c
Daniel Sehaik <danielsehaik@gmail.com>
2019-02-18 14:02:45 +0200

    created functions

--- OptimalDiscreteSlicing/Program.cs
+++ OptimalDiscreteSlicing/Program.cs
@@ -9,12 +9,23 @@ namespace OptimalDiscreteSlicing
 {
     class Program
     {
-        static void Main(string[] args)
+        static Dictionary<Tuple<int, int>, int> ErrorCalculation()
         {
+            Dictionary<Tuple<int,int>,int> ErrorDic = new Dictionary<Tuple<int,int>,int>();
+            
+            
+            
+            return ErrorDic;
+        }
 
-            Console.WriteLine("Hello Daniel");
-            Console.Write(typeof(string).Assembly.ImageRuntimeVersion);
-            DMesh3 mesh = StandardMeshReader.ReadMesh("C:\\Users\\Daniel\\Desktop\\bunny.obj");
+        static Vector3i getCordinate(int x, int y, int z)
+        {
+            return new Vector3i(x, y, z);
+        }
+
+        static Bitmap3 createVoxelizedRepresentation(String objPath)
+        {
+            DMesh3 mesh = StandardMeshReader.ReadMesh(objPath);
 
             int num_cells = 128;
             double cell_size = mesh.CachedBounds.MaxDim / num_cells;
@@ -31,10 +42,49 @@ namespace OptimalDiscreteSlicing
                 //for bunny only removes bottom
                 if (idx.y < 8)
                 {
+                    ////Console.WriteLine(bmp.Dimensions);
+                    //Vector3i v = new Vector3i();
+                    //v.x = 4;
+                    //v.y = 3;
+                    //v.z = 4;
+                    ////Console.WriteLine(bmp.Get(v));
                     bmp.Set(idx, false);
                 }
             }
+            return bmp;
+        }
 
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Daniel");
+            //DMesh3 mesh = StandardMeshReader.ReadMesh("C:\\Users\\Daniel\\Desktop\\bunny.obj");
+
+            //int num_cells = 128;
+            //double cell_size = mesh.CachedBounds.MaxDim / num_cells;
+
+            //MeshSignedDistanceGrid sdf = new MeshSignedDistanceGrid(mesh, cell_size);
+            //sdf.Compute();
+
+            ////** voxels**//
+            //Bitmap3 bmp = new Bitmap3(sdf.Dimensions);
+            //foreach (Vector3i idx in bmp.Indices())
+            //{
+            //    float f = sdf[idx.x, idx.y, idx.z];
+            //    bmp.Set(idx, (f < 0) ? true : false);
+            //    //for bunny only removes bottom
+            //    if (idx.y < 8)
+            //    {
+            //        Console.WriteLine(bmp.Dimensions);
+            //        Vector3i v = new Vector3i();
+            //        v.x = 4;
+            //        v.y = 3;
+            //        v.z = 4;
+            //        Console.WriteLine(bmp.Get(v));
+            //        bmp.Set(idx, false);
+            //    }
+            //}
+            Bitmap3 bmp = createVoxelizedRepresentation("C:\\Users\\Daniel\\Desktop\\bunny.obj");
             VoxelSurfaceGenerator voxGen = new VoxelSurfaceGenerator();
             voxGen.Voxels = bmp;
             //voxGen.ColorSourceF = (idx) =>
@@ -48,19 +98,19 @@ namespace OptimalDiscreteSlicing
             //** voxels**//
 
 
-            var iso = new DenseGridTrilinearImplicit(sdf.Grid, sdf.GridOrigin, sdf.CellSize);
+            //var iso = new DenseGridTrilinearImplicit(sdf.Grid, sdf.GridOrigin, sdf.CellSize);
 
-            MarchingCubes c = new MarchingCubes();
-            c.Implicit = iso;
-            c.Bounds = mesh.CachedBounds;
+            //MarchingCubes c = new MarchingCubes();
+            //c.Implicit = iso;
+            //c.Bounds = mesh.CachedBounds;
 
-            c.CubeSize = c.Bounds.MaxDim / 128;
-            c.Bounds.Expand(3 * c.CubeSize);
-            c.Generate();
+            //c.CubeSize = c.Bounds.MaxDim / 128;
+            //c.Bounds.Expand(3 * c.CubeSize);
+            //c.Generate();
 
-            DMesh3 outputMesh = c.Mesh;
+            //DMesh3 outputMesh = c.Mesh;
 
-            StandardMeshWriter.WriteMesh("C:\\Users\\Daniel\\Desktop\\output.obj", c.Mesh, WriteOptions.Defaults);
+            //StandardMeshWriter.WriteMesh("C:\\Users\\Daniel\\Desktop\\output.obj", c.Mesh, WriteOptions.Defaults);
         }
     }
 }
