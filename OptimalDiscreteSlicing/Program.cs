2694c218b7d3d82943281a8f4c0133e075746734
Daniel Sehaik <danielsehaik@gmail.com>
2019-02-18 14:21:46 +0200

    refactoring

--- OptimalDiscreteSlicing/Program.cs
+++ OptimalDiscreteSlicing/Program.cs
@@ -39,6 +39,7 @@ namespace OptimalDiscreteSlicing
             {
                 float f = sdf[idx.x, idx.y, idx.z];
                 bmp.Set(idx, (f < 0) ? true : false);
+                
                 //for bunny only removes bottom
                 if (idx.y < 8)
                 {
@@ -54,46 +55,24 @@ namespace OptimalDiscreteSlicing
             return bmp;
         }
 
+        static void printVoxelizedRepresentation(Bitmap3 bmp, String outputPath){
+            VoxelSurfaceGenerator voxGen = new VoxelSurfaceGenerator();
+            voxGen.Voxels = bmp;
+            voxGen.ColorSourceF = (idx) =>
+            {
+                return new Colorf((float)idx.x, (float)idx.y, (float)idx.z) * (1.0f / 4);
+            };
+            voxGen.Generate();
+            DMesh3 voxMesh = voxGen.Meshes[0];
+            Util.WriteDebugMesh(voxMesh, outputPath);
+        }
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("Hello Daniel");
-            //DMesh3 mesh = StandardMeshReader.ReadMesh("C:\\Users\\Daniel\\Desktop\\bunny.obj");
-
-            //int num_cells = 128;
-            //double cell_size = mesh.CachedBounds.MaxDim / num_cells;
-
-            //MeshSignedDistanceGrid sdf = new MeshSignedDistanceGrid(mesh, cell_size);
-            //sdf.Compute();
-
-            ////** voxels**//
-            //Bitmap3 bmp = new Bitmap3(sdf.Dimensions);
-            //foreach (Vector3i idx in bmp.Indices())
-            //{
-            //    float f = sdf[idx.x, idx.y, idx.z];
-            //    bmp.Set(idx, (f < 0) ? true : false);
-            //    //for bunny only removes bottom
-            //    if (idx.y < 8)
-            //    {
-            //        Console.WriteLine(bmp.Dimensions);
-            //        Vector3i v = new Vector3i();
-            //        v.x = 4;
-            //        v.y = 3;
-            //        v.z = 4;
-            //        Console.WriteLine(bmp.Get(v));
-            //        bmp.Set(idx, false);
-            //    }
-            //}
             Bitmap3 bmp = createVoxelizedRepresentation("C:\\Users\\Daniel\\Desktop\\bunny.obj");
-            VoxelSurfaceGenerator voxGen = new VoxelSurfaceGenerator();
-            voxGen.Voxels = bmp;
-            //voxGen.ColorSourceF = (idx) =>
-            //{
-            //    return new Colorf((float)idx.x, (float)idx.y, (float)idx.z) * (1.0f / 4);
-            //};
-            voxGen.Generate();
-            DMesh3 voxMesh = voxGen.Meshes[0];
-            Util.WriteDebugMesh(voxMesh, "C:\\Users\\Daniel\\Desktop\\outputVox.obj");
+            printVoxelizedRepresentation(bmp, "C:\\Users\\Daniel\\Desktop\\outputVox.obj");
 
             //** voxels**//
 
